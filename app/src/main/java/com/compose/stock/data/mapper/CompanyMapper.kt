65877e858591343c854fb3940d39651b2fd795eb
Kavya Athmuri <kathmuri@mobiquityinc.com>
2022-04-17 11:21:58 +0530

    Retrofit api classes setup Room data base classes setup Add the gradle file to easy understand way

---
+++ app/src/main/java/com/compose/stock/data/mapper/CompanyMapper.kt
@@ -0,0 +1,20 @@
+package com.compose.stock.data.mapper
+
+import com.compose.stock.data.local.CompanyListingEntity
+import com.compose.stock.domain.model.CompanyListing
+
+fun CompanyListingEntity.toCompanyListing():CompanyListing {
+    return CompanyListing(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
+
+fun CompanyListing.toCompanyListingEntity():CompanyListingEntity {
+    return CompanyListingEntity(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
---
+++ app/src/main/java/com/compose/stock/data/remote/StockApi.kt
@@ -0,0 +1,15 @@
+package com.compose.stock.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface StockApi {
+
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getListings(
+        @Query("apikey") apiKey: String
+    ): ResponseBody
+
+}
